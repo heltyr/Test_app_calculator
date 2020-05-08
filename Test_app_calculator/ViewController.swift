a205ff33a8d4e5eaad18ecb5796c6a3a032e74e4
heltyr <kirill95.09@mail.ru>
2020-05-08 13:37:33 +0300

    Initial Commit

---
+++ Test_app_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_app_calculator
+//
+//  Created by Heltyr911 on 5/8/20.
+//  Copyright © 2020 heltyr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_app_calculatorTests/Test_app_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_app_calculatorTests.swift
+//  Test_app_calculatorTests
+//
+//  Created by Heltyr911 on 5/8/20.
+//  Copyright © 2020 heltyr. All rights reserved.
+//
+
+import XCTest
+@testable import Test_app_calculator
+
+class Test_app_calculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
